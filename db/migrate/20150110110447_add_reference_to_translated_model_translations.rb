8de8c9dee13f325a4ecf0ed3e0092188c335eb42
Gregory Romé <gregory.rome@gmail.com>
2015-01-12 21:23:55 +0100

    fixes index

--- db/migrate/20150110110447_add_reference_to_translated_model_translations.rb
+++ db/migrate/20150110110447_add_reference_to_translated_model_translations.rb
@@ -2,6 +2,6 @@ class AddReferenceToTranslatedModelTranslations < ActiveRecord::Migration
   def change
     add_reference :translated_model_translations, :translated, polymorphic: true
     remove_index :translated_model_translations, :key
-    add_index :translated_model_translations, [:translated, :key], unique: true
+    add_index :translated_model_translations, [:translated_id, :translated_type, :key], name: :translated_idx, unique: true
   end
 end
